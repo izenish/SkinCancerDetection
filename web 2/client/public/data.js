23b094a26be429081a9e701bbf5a388748c8d6c2
izenish <zenish77@gmail.com>
2022-08-20 12:41:00 +0545

    Model changed with UI enhancement

---
+++ web 2/client/public/data.js
@@ -0,0 +1,76 @@
+export const cancer_types = {
+    'Actinic keratoses' :{
+        info:'Actinic keratoses (also called solar keratoses) are dry scaly patches of skin that have been damaged by the sun. <br/> The patches are not usually serious. But there\'s a small chance they could become skin cancer, so it\'s important to avoid further damage to your skin.',
+        symptoms:[
+            'Rough, dry or scaly patch of skin, usually less than 1 inch (2.5 centimeters) in diameter',
+            'Flat to slightly raised patch or bump on the top layer of skin',
+            'In some cases, a hard, wartlike surface',
+            'Color variations, including pink, red or brown',
+            'Itching, burning, bleeding or crusting',
+            'New patches or bumps on sun-exposed areas of the head, neck, hands and forearms'
+        ],
+        link:'https://www.nhs.uk/conditions/actinic-keratoses/'
+    },
+    'Melanocytic nevi(nv)':{
+        info:'Melanocytic nevi are benign neoplasms or hamartomas composed of melanocytes, the pigment-producing cells that constitutively colonize the epidermis. Melanocytes are derived from the neural crest and migrate during embryogenesis to selected ectodermal sites (primarily the skin and the CNS), but also to the eyes and the ears. Ectopic melanocytes have been identified at autopsy in the gastrointestinal and genitourinary tracts. Congenital melanocytic nevi are thought to represent an anomaly in embryogenesis and, as such, could be considered, at least in a sense, malformations or hamartomas.',
+        symptoms:[
+            'There are usually no symptoms with congenital melanocytic nevi.  Sometimes the larger ones may itch.',
+            'If pain, severe or persistent itching, bleeding, or crusting develop, see your child’s doctor.'
+        ],
+        link:'https://emedicine.medscape.com/article/1058445-overview'
+    }   ,
+    'melanoma':{
+        info:'Melanoma, the most serious type of skin cancer, develops in the cells (melanocytes) that produce melanin — the pigment that gives your skin its color. Melanoma can also form in your eyes and, rarely, inside your body, such as in your nose or throat.',
+        symptoms:[
+            'Melanomas can develop anywhere on your body. They most often develop in areas that have had exposure to the sun, such as your back, legs, arms and face',
+           'Asymmetry. The shape of one-half of the mole does not match the other.',
+            'Border. The edges are ragged, notched, uneven, or blurred.',
+            'Color. Shades of black, brown, and tan may be present. Areas of white, gray, red, or blue may also be seen.',
+            'Diameter. The diameter is usually larger than 6 millimeters (mm) or has grown in size. This is about 1/4 inch in diameter, about the size of a pencil eraser. Melanoma may be smaller when it is first detected.'
+
+        ],
+        link:'https://www.mayoclinic.org/diseases-conditions/melanoma/symptoms-causes/syc-20374884#:~:text=Melanoma%2C%20the%20most%20serious%20type,in%20your%20nose%20or%20throat.'
+    },
+    'Dermatofibroma':{
+        info:'Dermatofibroma is a commonly occurring cutaneous entity usually centered within the skin\'s dermis. Dermatofibromas are referred to as benign fibrous histiocytomas of the skin, superficial/cutaneous benign fibrous histiocytomas, or common fibrous histiocytoma.',
+        symptoms:[
+            'Appearance: A dermatofibroma presents as a round bump that is mostly under the skin.',
+            'Size: The normal range is about 0.5–1.5 centimeters (cm), with most lesions being 0.7–1.0 cm in diameter. The size will usually remain stable.',
+            'Color: The growths vary in color among individuals but will generally be pink, red, gray, brown, or black',
+            'Location: Dermatofibromas are most common on the legs, but they sometimes appear on the arms, trunk, and, less commonly, elsewhere on the body.',
+            'Additional symptoms: Although they are usually harmless and painless, these growths may occasionally be itchy, tender, painful, or inflamed',
+        ],
+        link:'https://www.ncbi.nlm.nih.gov/books/NBK470538/#:~:text=Dermatofibroma%20is%20a%20commonly%20occurring,histiocytomas%2C%20or%20common%20fibrous%20histiocytoma.'
+    },
+    'Vascular lesions':{
+        info:'Vascular lesions are relatively common abnormalities of the skin and underlying tissues, more commonly known as birthmarks. There are three major categories of vascular lesions: Hemangiomas, Vascular Malformations, and Pyogenic Granulomas.',
+        symptoms:[
+            'They appear as rapidly-growing, red, and often ulcerating bump or polyp-like masses which bleed easily when disturbed.'
+        ],
+        link:'https://www.ssmhealth.com/cardinal-glennon/pediatric-plastic-reconstructive-surgery/hemangiomas'
+    },
+    'Basal cell carcinoma':{
+        info:'Basal cell carcinoma is a type of skin cancer. Basal cell carcinoma begins in the basal cells — a type of cell within the skin that produces new skin cells as old ones die off.',
+        link:'https://www.mayoclinic.org/diseases-conditions/basal-cell-carcinoma/symptoms-causes/syc-20354187#:~:text=on%20brown%20skin-,Basal%20cell%20carcinoma%20is%20a%20type%20of%20skin%20cancer%20that,a%20type%20of%20skin%20cancer.',
+        symptoms:[
+            'A shiny, skin-colored bump that\'s translucent, meaning you can see a bit through the surface. The bump can look pearly white or pink on white skin. On brown and Black skin, the bump often looks brown or glossy black. Tiny blood vessels might be visible, though they may be difficult to see on brown and Black skin. The bump may bleed and scab over.'
+     ,
+     'A brown, black or blue lesion — or a lesion with dark spots — with a slightly raised, translucent border.',
+    ' A flat, scaly patch with a raised edge. Over time, these patches can grow quite large.',
+    'A white, waxy, scar-like lesion without a clearly defined border.'
+       ]
+    },
+    'Benign keratosis-like lesions':{
+        info:'A seborrheic keratosis (seb-o-REE-ik ker-uh-TOE-sis) is a common noncancerous (benign) skin growth. People tend to get more of them as they get older.  Seborrheic keratoses are usually brown, black or light tan. The growths (lesions) look waxy or scaly and slightly raised. They appear gradually, usually on the face, neck, chest or back.',
+        symptoms:[
+            'A round or oval-shaped waxy or rough bump, typically on the face, chest, a shoulder or the back',
+            'A flat growth or a slightly raised bump with a scaly surface, with a characteristic "pasted on" look',
+            'Varied size, from very small to more than 1 inch (2.5 centimeters) across',
+            'Varied number, ranging from a single growth to multiple growths',
+            'Very small growths clustered around the eyes or elsewhere on the face, sometimes called flesh moles or dermatosis papulosa nigra, common on Black or brown skin',
+            'Varied in color, ranging from light tan to brown or black'
+        ],
+        link:'https://www.mayoclinic.org/diseases-conditions/seborrheic-keratosis/symptoms-causes/syc-20353878#:~:text=A%20seborrheic%20keratosis%20(seb%2Do,or%20scaly%20and%20slightly%20raised.'
+    }
+
+}
